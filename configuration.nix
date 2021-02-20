c3741463fa012c45e00c419cb6c2218e1c524f64
Jan Kaifer <jan@kaifer.cz>
2021-02-20 23:08:10 +0100

    Added basic configuration

---
+++ configuration.nix
@@ -0,0 +1,157 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # fix errors in hardware-configuration.nix
+  boot.kernelModules = [ "iwlwifi" ];
+  hardware.enableRedistributableFirmware = true;
+  # fileSystems."/" = {
+  #   device = "/dev/VolGroup00/lvolnixos";
+  #   fsType = "ext4";
+  # };
+  # swapDevices = [
+  #   { device = "/dev/VolGroup00/swap; }
+  # ];
+
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "c9"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Prague";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.wlp0s20f3.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "ter-i32b";
+    # keyMap = "us";
+    useXkbConfig = true;
+    earlySetup = true;
+    packages = with pkgs; [ terminus_font ];
+  };
+
+  services.xserver = {
+    enable = true;
+    desktopManager.xterm.enable = false;
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs."i3-gaps";
+      configFile = ./configs/i3.conf;
+    };
+    libinput.enable = true;
+  };
+
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "caps:escape";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.pearman = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # CLI Utils
+    wget
+    iw
+    tree
+    lshw
+    git
+
+    # Cli Apps
+    vim
+    htop
+
+    # GUI Tools
+    arandr
+
+    # GUI Apps
+    firefox
+    google-chrome
+    gitkraken
+    zoom-us
+    discord
+    slack-dark
+    vscode
+  ];
+
+  environment.variables = {
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_CACHE_HOME = "$HOME/.cache";
+
+    ANDROID_HOME = "$HOME/Android/Sdk"; # Set the home of the android SDK
+    
+    TERMINAL = "kitty";
+    BROWSER = "google-chrome-stable";
+  };
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+  fonts.fonts = with pkgs; [
+    fira-code
+    fira-code-symbols
+  ];
+}
+
