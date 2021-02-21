38aefd3b5cfa392dfd37de8a3f668ccebe95fa4d
Jan Kaifer <jan@kaifer.cz>
2021-02-21 22:13:01 +0100

    added arandr 4k monitor

---
+++ configs/.screenlayout/4k-multimonitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2160 --pos 0x2160 --rotate normal --output DP-1 --off --output DP-2 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-3 --off
