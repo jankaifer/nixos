0be21cd43920491b4930bc4dcb624b36b5cc6793
Jan Kaifer <jan@kaifer.cz>
2022-09-16 13:10:25 +0200

    TYPO

--- hardware/framework.nix
+++ hardware/framework.nix
@@ -1,7 +1,7 @@
 { pkgs, ...}:
 
 {
-  # Device specifi config
+  # Device specific config
   
   ## Enable swap on luks
   boot.initrd.luks.devices."luks-03537895-0d55-4d42-83b0-28f2c82e6273".device = "/dev/disk/by-uuid/03537895-0d55-4d42-83b0-28f2c82e6273";
