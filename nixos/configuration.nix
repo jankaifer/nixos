03181601cdc5e8623f7d8aeb08c679eb77bd3e9e
Jan Kaifer <jan@kaifer.cz>
2022-09-28 12:52:15 +0200

    applied change recommended by zsa

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -52,7 +52,7 @@ in
   services.udev.extraRules = ''
     # Rules for Oryx web flashing and live training
     KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
-    KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0666", GROUP="plugdev"
 
     # Legacy rules for live training over webusb (Not needed for firmware v21+)
     # Rule for all ZSA keyboards
