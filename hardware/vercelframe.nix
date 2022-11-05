5abf98e8017a6290e483c177985c833f26eb8ad3
Jan Kaifer <jan@kaifer.cz>
2022-11-05 12:15:51 +0000

    fix vercel frame disk setup

--- hardware/vercelframe.nix
+++ hardware/vercelframe.nix
@@ -6,9 +6,8 @@
     ./framework.nix
   ];
 
-  ## Enable swap on luks
-  boot.initrd.luks.devices."luks-03537895-0d55-4d42-83b0-28f2c82e6273".device = "/dev/disk/by-uuid/03537895-0d55-4d42-83b0-28f2c82e6273";
-  boot.initrd.luks.devices."luks-03537895-0d55-4d42-83b0-28f2c82e6273".keyFile = "/crypto_keyfile.bin";
+  ## Make sure that /var/log is moun ted early enough
+  fileSystems."/var/log".neededForBoot = true;
 
   ## Host name
   networking.hostName = "vercelframe";
