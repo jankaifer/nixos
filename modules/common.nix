26883e43284fb2f831c051cc0ae5bcf5f1e8e068
Jan Kaifer <jan@kaifer.cz>
2022-11-22 21:24:15 +0100

    fix .node-version

--- modules/common.nix
+++ modules/common.nix
@@ -213,7 +213,7 @@ in
       # For some reason vscode can't read the config when provided by impermanence
       home.file = {
         ".vimrc".source = ./dotfiles/vim/.vimrc;
-        ".node-version" = "v18.3.0";
+        ".node-version".text = "v18.3.0";
       };
     };
   };
