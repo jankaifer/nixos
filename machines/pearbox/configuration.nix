648fa4881339f4c7910ef4594f3bd4516bb0f429
Jan Kaifer <jan@kaifer.cz>
2022-11-28 21:39:51 +0100

    typo

--- machines/pearbox/configuration.nix
+++ machines/pearbox/configuration.nix
@@ -17,7 +17,7 @@
   networking.hostName = "pearbox";
 
   # Wallpaper
-  home-manager.users.pearman.dconf.settings."org/gnome/desktop/background".picture-uri = "file://" + ../../wallpapers/prism-rabbit.png;
+  home-manager.users.pearman.dconf.settings."org/gnome/desktop/background".picture-uri = "file://" + ../../wallpapers/prism-rabbit.jpg;
 
   # Options
   custom = {
