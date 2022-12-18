b22b5013217e2bebd97526ae2ee78f27ece41b3e
Jan Kaifer <jan@kaifer.cz>
2022-12-18 22:30:36 +0100

    switch zsa to upstream config

--- modules/zsa.nix
+++ modules/zsa.nix
@@ -15,16 +15,6 @@
   config =
     lib.mkIf config.custom.zsa.enable
       {
-        # system option is outdated
-        hardware.keyboard.zsa.enable = false;
-
-        users.groups = {
-          plugdev = { };
-        };
-        users.users.pearman.extraGroups = [ "plugdev" ];
-        services.udev.extraRules = ''
-          KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0666", GROUP="plugdev"
-          KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0666", GROUP="plugdev"
-        '';
+        hardware.keyboard.zsa.enable = true;
       };
 }
