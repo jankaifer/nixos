553de9fc7a2490ef87978f2ecbc5292477364dc0
Jan Kaifer <jan@kaifer.cz>
2023-01-02 18:18:56 +0100

    intall virt manager

--- modules/common.nix
+++ modules/common.nix
@@ -255,6 +255,12 @@ in
       };
     };
   };
+
+  # Virt manager
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true;
+  environment.systemPackages = [ pkgs.virt-manager ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
