b2f2e0bfd9f11a2df9135fae5fe759be340941dc
Jan Kaifer <jan@kaifer.cz>
2023-01-14 22:39:36 +0100

    finish up fan noise fix

--- modules/framework.nix
+++ modules/framework.nix
@@ -20,15 +20,8 @@
       boot.kernelParams = [
         # Fix brightness keys (https://dov.dev/blog/nixos-on-the-framework-12th-gen)
         "module_blacklist=hid_sensor_hub"
-        # In case your laptop hangs randomly (https://nixos.wiki/wiki/Bootloader)
-        "intel_idle.max_cstate=1"
       ];
 
-      # My framework started overheating - it has  needlessly high frequency
-      # auto-cpufreq mitigates that a bit - it makes fan noise bearable
-      services.auto-cpufreq.enable = true;
       services.thermald.enable = true;
-      # Powerprofiles seem useless
-      services.power-profiles-daemon.enable = false;
     };
 }
