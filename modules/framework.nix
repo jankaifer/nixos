cc8fae4a7809c02995f074be0f9edb299f1842d3
Jan Kaifer <jan@kaifer.cz>
2023-01-14 19:18:33 +0100

    wip

--- modules/framework.nix
+++ modules/framework.nix
@@ -23,5 +23,10 @@
         # In case your laptop hangs randomly (https://nixos.wiki/wiki/Bootloader)
         "intel_idle.max_cstate=1"
       ];
+
+      # Thermal stuff from: https://www.reddit.com/r/NixOS/comments/10adqyb/what_do_people_use_to_manage_their_cpu_frequency/
+      # powerManagement.cpuFreqGovernor = "powersave";
+      services.auto-cpufreq.enable = true;
+      services.thermald.enable = true;
     };
 }
