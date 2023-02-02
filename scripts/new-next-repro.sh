d83708b2d1aeb85da3877e7fc3d9d3c9c37bd7da
Jan Kaifer <jan@kaifer.cz>
2023-02-02 16:03:49 +0100

    update default reproduction to app dir

--- scripts/new-next-repro.sh
+++ scripts/new-next-repro.sh
@@ -6,10 +6,11 @@ set -o xtrace # print all commands
 ISSUE_NUM="$1"
 GH_REPO_NAME="next-repro-$ISSUE_NUM"
 LOCAL_NAME="$ISSUE_NUM"
+TEMPLATE="reproduction-template-app"
 
 gh repo create --clone --description "Reproduction of https://github.com/vercel/next.js/issues/$ISSUE_NUM" --public "$GH_REPO_NAME"
 mv "$GH_REPO_NAME" "$LOCAL_NAME"
-steam-run pnpm create next-app --example reproduction-template "$LOCAL_NAME"
+pnpm create next-app --example "$TEMPLATE" "$LOCAL_NAME"
 cd "$LOCAL_NAME"
 git add --all
 git commit -m "init"
