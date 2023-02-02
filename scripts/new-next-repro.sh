9d83407af3f74ad80f7f00c50b0c0964db168b0c
Jan Kaifer <jan@kaifer.cz>
2023-02-02 16:08:59 +0100

    fix tempalte name

--- scripts/new-next-repro.sh
+++ scripts/new-next-repro.sh
@@ -6,7 +6,7 @@ set -o xtrace # print all commands
 ISSUE_NUM="$1"
 GH_REPO_NAME="next-repro-$ISSUE_NUM"
 LOCAL_NAME="$ISSUE_NUM"
-TEMPLATE="reproduction-template-app"
+TEMPLATE="reproduction-template-app-dir"
 
 gh repo create --clone --description "Reproduction of https://github.com/vercel/next.js/issues/$ISSUE_NUM" --public "$GH_REPO_NAME"
 mv "$GH_REPO_NAME" "$LOCAL_NAME"
