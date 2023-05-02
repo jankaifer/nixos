3e143b43192864e8519d64efcd49f0289e24a313
Jan Kaifer <jan@kaifer.cz>
2023-05-02 18:04:45 +0200

    typo

--- modules/gui.nix
+++ modules/gui.nix
@@ -193,7 +193,7 @@ in
           # Hyper is broken on latest unstable: https://github.com/NixOS/nixpkgs/issues/105961
           stablePkgs.hyper
           # Hyper is broken on latest unstable: https://github.com/NixOS/nixpkgs/issues/105961
-          stablePhgs.insomnia
+          stablePkgs.insomnia
 
           # Electron evil apps
           atom
