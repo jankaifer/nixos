c24ce6884f2da05b84ed0dc2b167b2d1cf8ab2b6
Jan Kaifer <jan@kaifer.cz>
2023-05-06 23:26:57 +0200

    syntax error fix

--- modules/gui.nix
+++ modules/gui.nix
@@ -130,7 +130,7 @@ in
           "org/gnome/desktop/interface"."show-battery-percentage" = true;
 
           # Night light
-          "org/gnome/settings-daemon/plugins/color"."night-light-temperature = 2300;
+          "org/gnome/settings-daemon/plugins/color"."night-light-temperature" = 2300;
 
           # Analytics
           "org/gnome/desktop/privacy"."report-technical-problems" = true;
