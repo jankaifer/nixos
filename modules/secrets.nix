afae421d77e1726bd52ca80a6ce0f9f2fc813b1a
Jan Kaifer <jan@kaifer.cz>
2023-10-30 22:07:50 +0100

    load agenix ssh key from /persist when using impermanence

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -1,7 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
-  age.identityPaths = [ "/home/${config.custom.options.username}/.ssh/id_ed25519" ];
+  age.identityPaths =
+    if config.custom.impermanence.enable then [
+      "/persist/home/${config.custom.options.username}/.ssh/id_ed25519"
+    ] else [
+      "/home/${config.custom.options.username}/.ssh/id_ed25519"
+    ];
   age.secrets.login-password.file = ../secrets/login-password.age;
   age.secrets.wifi-passwords.file = ../secrets/wifi-passwords.age;
 }
