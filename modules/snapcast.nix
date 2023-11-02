7262bd27acc3ad42efa9f9589a2bea527630af69
Jan Kaifer <jan@kaifer.cz>
2023-11-02 17:25:16 +0100

    temorarily disable snapcast

--- modules/snapcast.nix
+++ modules/snapcast.nix
@@ -15,27 +15,27 @@ in
 
   config = lib.mkIf config.custom."${option-name}".enable {
     services.snapserver = {
-      enable = true;
-      openFirewall = true;
-      http = {
-        enable = true;
-        listenAddress = "0.0.0.0";
-        docRoot = "${pkgs.snapcast}/share/snapserver/snapweb/";
-      };
-      streams = {
-        Spotify = {
-          type = "librespot";
-          location = "${pkgs.librespot}/bin/librespot";
-          query = {
-            devicename = "Snapcast";
-            normalize = "true";
-            autoplay = "false";
-            cache = "/home/${config.custom.options.username}/.cache/librespot";
-            killall = "true";
-            params = "--cache-size-limit=4G";
-          };
-        };
-      };
+      # enable = true;
+      # openFirewall = true;
+      # http = {
+      #   enable = true;
+      #   listenAddress = "0.0.0.0";
+      #   docRoot = "${pkgs.snapcast}/share/snapserver/snapweb/";
+      # };
+      # streams = {
+      #   Spotify = {
+      #     type = "librespot";
+      #     location = "${pkgs.librespot}/bin/librespot";
+      #     query = {
+      #       devicename = "Snapcast";
+      #       normalize = "true";
+      #       autoplay = "false";
+      #       cache = "/home/${config.custom.options.username}/.cache/librespot";
+      #       killall = "true";
+      #       params = "--cache-size-limit=4G";
+      #     };
+      #   };
+      # };
     };
 
     # pass Spotify credentials to librespot with a file containing:
