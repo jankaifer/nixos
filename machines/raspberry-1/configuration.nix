533c5b59704b04aa9ae1bc9772d07585aa6e141a
Jan Kaifer <jan@kaifer.cz>
2023-11-02 22:45:36 +0100

    use snapcast on raspberry-1

--- machines/raspberry-1/configuration.nix
+++ machines/raspberry-1/configuration.nix
@@ -1,7 +1,12 @@
-{ lib, ... }: {
+{ lib, config, pkgs, ... }: {
   imports = [
     ../../modules
     ../../modules/raspberry-shared.nix
+  ];
+
+
+  environment.systemPackages = [
+    pkgs.snapcast
   ];
 
   custom = {
