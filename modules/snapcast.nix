2afc568815042c258d5bbe2dd4fac50034f6ec8e
Jan Kaifer <jan@kaifer.cz>
2023-11-08 23:22:54 +0100

    fix issues with custom webcast

--- modules/snapcast.nix
+++ modules/snapcast.nix
@@ -21,7 +21,7 @@ in
       http = {
         enable = true;
         listenAddress = "0.0.0.0";
-        docRoot = /home/pearman/dev/jankaifer/snapweb/dist;
+        docRoot = "${/home/pearman/dev/jankaifer/snapweb/dist}";
       };
       streams = {
         Spotify = {
