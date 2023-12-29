5a24c4b95291dfa45ac95cdb865fab3fd73d406d
Jan Kaifer <jan@kaifer.cz>
2023-12-29 16:37:19 +0000

    initial oldbox config

---
+++ machines/oldbox/configuration.nix
@@ -0,0 +1,39 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+     ./hardware-configuration.nix
+     ../../modules
+  ];
+
+  # We need few volumes to be mounted before our system starts booting
+  fileSystems."/var/log".neededForBoot = true;
+  fileSystems."/persist".neededForBoot = true;
+  fileSystems."/".neededForBoot = true;
+  fileSystems."/home".neededForBoot = true;
+
+  # Root needs to have correct permissions otherwise openssh will complain and won't work
+  fileSystems."/".options = [ "mode=755" ];
+
+  home-manager.users.jankaifer.home.stateVersion = "22.05";
+
+  networking.firewall.enable = false;
+
+  # Options
+  custom = {
+    cli-server.enable = true;
+    common.enable = true;
+    impermanence.enable = true;
+    fck.enable = true;
+    ssh-server.enable = true;
+    ssh-keys-autogenerate.enable = true;
+
+    options = {
+      hostName = "oldbox";
+    };
+  };
+}
