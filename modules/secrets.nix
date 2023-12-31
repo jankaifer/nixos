f380150dc7394e41aa98d3ebd7bd5ae2841750a5
Jan Kaifer <jan@kaifer.cz>
2023-12-31 12:43:13 +0100

    fix agenix identities search

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -5,6 +5,10 @@
     "/home/${config.custom.options.username}/.ssh/id_ed25519"
     # if using impermanence it might have not loaded yet
     "/persist/home/${config.custom.options.username}/.ssh/id_ed25519"
+
+    # We need to make sure we find identities when installing on a new system
+    "/mnt/home/${config.custom.options.username}/.ssh/id_ed25519"
+    "/mnt/persist/home/${config.custom.options.username}/.ssh/id_ed25519"
   ];
   age.secrets.login-password.file = ../secrets/login-password.age;
   age.secrets.wifi-passwords.file = ../secrets/wifi-passwords.age;
