3734c6cb161f598e9ff68b788d32fb78a8961fb0
Jan Kaifer <jan@kaifer.cz>
2024-01-28 22:32:45 +0100

    inline push to cf script

--- machines/oldbox/default.nix
+++ machines/oldbox/default.nix
@@ -444,9 +444,52 @@ in
     description = "Measure internet speed with cfspeedtest and export to victoriametrics";
     after = [ "network-online.target" ];
     path = [ pkgs.nix ]; # This is needed so that nix-shell works
-    script = ''
-      ${pkgs.cfspeedtest}/bin/cfspeedtest -o json | ${../../scripts/push-cf-speed-test-result-to-vm.js} "https://victoriametrics.${domain}/api/v1/import/prometheus"
-    '';
+    script =
+      let
+        vmUrl = "https://victoriametrics.${domain}/api/v1/import/prometheus";
+        exportToVmScript = pkgs.writeText "export.js" ''
+          const vmUrl = "${vmUrl}";
+
+          const data = JSON.parse(await Bun.stdin.text())
+          console.log("We got the following data from speed test:")
+          console.log(data)
+          console.log()
+
+          const serializeTest = (testData) => ["min", "q1", "median", "q3", "max", "avg"]
+            .map(percentile => [
+              'cf_speed_test_',
+              testData.test_type.toLowerCase(),
+              '_',
+              percentile,
+              ' }{payload_size="'
+              testData.payload_size,
+              '"} '
+              testData[percentile],
+              '}'
+              ].map(String).join("")
+            )
+            .join("\n")
+          const metrics = data.map(serializeTest).join("\n")
+
+          console.log("Uploading to ", vmUrl)
+          console.log("Metrics being uploaded:")
+          console.log(metrics)
+          console.log()
+
+          await fetch(vmUrl, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'text/plain'
+              },
+              body: metrics.trim(),
+          })
+
+          console.log("Done")
+        '';
+      in
+      ''
+        '${pkgs.cfspeedtest}/bin/cfspeedtest' -o json | ${pkgs.bun}/bin/bun '${exportToVmScript}'
+      '';
   };
 
   systemd.timers.cfspeedtest-metric-exporter = {
