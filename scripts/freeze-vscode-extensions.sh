e7ec87b3cee6f3f63bca4c6b299e6a9f7132995a
Jan Kaifer <jan@kaifer.cz>
2024-03-02 10:19:06 +0100

    update extensions

--- scripts/freeze-vscode-extensions.sh
+++ scripts/freeze-vscode-extensions.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 NIXOS_PATH="/persist/$HOME/dev/jankaifer/nixos"
-EXTENSION_CONF="$NIXOS_PATH/modules/home-manager-custom/custom/vscode/extensions.nix"
+EXTENSION_CONF="$NIXOS_PATH/modules/home-manager/custom/vscode/extensions.nix"
 # EXTENSION_CONF="$NIXOS_PATH/modules/vscode/extensions.nix"
 
 exec "$NIXOS_PATH/scripts/print-frozen-vscode-extensions.sh" > "$EXTENSION_CONF"
--- scripts/update-vscode-extensions.sh
+++ scripts/update-vscode-extensions.sh
@@ -3,7 +3,18 @@
 set -euo pipefail
 
 NIXOS_PATH="/persist/$HOME/dev/jankaifer/nixos"
-EXTENSION_CONF="$NIXOS_PATH/modules/home-manager-custom/custom/vscode/extensions.nix"
-# EXTENSION_CONF="$NIXOS_PATH/modules/vscode/extensions.nix"
+EXTENSION_CONF="$NIXOS_PATH/modules/home-manager/custom/vscode/extensions.nix"
+UPDATE_SCRIPT_URL="https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/applications/editors/vscode/extensions/update_installed_exts.sh"
 
-exec "$NIXOS_PATH/modules/nixpkgs/pkgs/applications/editors/vscode/extensions/update_installed_exts.sh" > "$EXTENSION_CONF"
+tmpfile=$(mktemp)
+
+curl -s "$UPDATE_SCRIPT_URL" -o $tmpfile
+
+# Make the script executable
+chmod +x $tmpfile
+
+# Run the script
+"$tmpfile" > "$EXTENSION_CONF"
+
+# Clean up
+rm $tmpfile
