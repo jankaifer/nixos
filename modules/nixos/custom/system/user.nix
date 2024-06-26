6372081e131edc462c99115279cc4433831d04ec
Jan Kaifer <jan@kaifer.cz>
2024-06-26 21:30:08 +0200

    wip

--- modules/nixos/custom/system/user.nix
+++ modules/nixos/custom/system/user.nix
@@ -43,14 +43,17 @@ in
       defaultUserShell = pkgs.zsh;
       users.${cfg.user} = {
         isNormalUser = true;
-        hashedPasswordFile = config.age.secrets.login-password.path;
+        # hashedPasswordFile = config.age.secrets.login-password.path;
+        password = "jankaifer";
         extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" ];
         openssh.authorizedKeys.keys = lib.splitString "\n" (builtins.readFile inputs.myPublicSshKeys);
       };
     };
     age.identityPaths = [
       "/home/${cfg.user}/.ssh/id_ed25519"
-      "/persist/home/${cfg.user}/.ssh/id_ed25519"
+      "/persist/home/${cfg.user}/.ssh/id_ed25519"  
+      "/mnt/home/${cfg.user}/.ssh/id_ed25519"
+      "/mnt/persist/home/${cfg.user}/.ssh/id_ed25519"
     ];
     programs.neovim = {
       enable = true;
