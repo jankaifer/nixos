64b58a4ca269a0f977206724af5329c719b40adf
Jan Kaifer <jan@kaifer.cz>
2024-07-12 18:24:59 +0200

    oldbox: get rid of coolify

--- machines/oldbox/default.nix
+++ machines/oldbox/default.nix
@@ -819,48 +819,9 @@ in
   };
 
   # Add Coolify
-  # This key was generated on first start of coolify
   # Coolify needs root login
   services.openssh.settings.PermitRootLogin = "prohibit-password";
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdae7j5X3pyHPTqeRcEyz/Sqjhe5zro0jmicwiHONSp/0UWTRE2l2uOlgzw6/5T2da8Jxr53MsPrEH/t9jAlZf+pt7xKgJWm7KYWJKJn5ipBil66lQoI4Hdh1E4fFdz8YmZYOis24GFntPc9sqszyDmrG3RuHsR6HPBN01AUAFNykoFOc/eDQ6iExXo2CGtfgtq7EQvp8AhLt7+yFcqdUaXsdokqDFfTJKrUpWyo6wrK9k0lP8aCR8Y8O5pwRdKgH3ocQ9f/+2tVgimMZ3L7Xf7cHH/pxqjYdwM3FpNw9hWbD7XCHYj/kI7lTiX3+uaRRkI4WHGa4SpyhxNpPPubA1 coolify-generated-ssh-key"
   ];
-
-  age.secrets.coolify-env.file = ../../secrets/coolify-env-file.age;
-  systemd.services.coolify-prepare-files = {
-    description = "Setup files for coolify";
-    wantedBy = [ "coolify.service" ];
-    wants = [ "data-coolify.mount" ];
-    script = ''
-      #! ${pkgs.bash}/bin/bash
-      NAMES='source ssh applications databases backups services proxy webhooks-during-maintenance ssh/keys ssh/mux proxy/dynamic'
-      for NAME in $NAMES
-      do
-        FOLDER_PATH="/data/coolify/$NAME"
-        if [ ! -d "$FOLDER_PATH" ]; then
-          mkdir -p "$FOLDER_PATH"
-        fi
-      done
-
-      cp -f "${./coolify/docker-compose.yml}" /data/coolify/source/docker-compose.yml
-      cp -f "${./coolify/docker-compose.prod.yml}" /data/coolify/source/docker-compose.prod.yml
-      cp -f "${ config.age.secrets.coolify-env.path }" /data/coolify/source/.env
-      cp -f "${./coolify/upgrade.sh}" /data/coolify/source/upgrade.sh
-
-      # Generate SSH key if not ready
-      if [ ! -f "/data/coolify/ssh/keys/id.root@host.docker.internal" ]; then
-        ssh-keygen -f /data/coolify/ssh/keys/id.root@host.docker.internal -t ed25519 -N "" -C root@coolify
-      fi
-
-      chown -R 9999:root /data/coolify
-      chmod -R 700 /data/coolify
-    '';
-  };
-  systemd.services.coolify = {
-    script = ''
-      APP_PORT="${toString coolify.port}" SOKETI_PORT="${toString coolify.websocketPort}" "${pkgs.docker}/bin/docker" compose --env-file /data/coolify/source/.env -f /data/coolify/source/docker-compose.yml -f /data/coolify/source/docker-compose.prod.yml up -d --pull always --remove-orphans --force-recreate
-    '';
-    after = [ "docker.service" "docker.socket" ];
-    wantedBy = [ "multi-user.target" ];
-  };
 }
