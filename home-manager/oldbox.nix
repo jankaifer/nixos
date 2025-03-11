be467e9e8ada5c7fde7b971205d9de04559c68eb
Jan Kaifer <jan@kaifer.cz>
2025-03-11 21:33:53 +0100

    drop gnome on oldbox

--- home-manager/oldbox.nix
+++ home-manager/oldbox.nix
@@ -1,17 +1,6 @@
 { pkgs, ... }:
 {
   custom = {
-    gnome = {
-      enable = true;
-      idleDelay = 0;
-      # for some reason nerdfonts can't be build on oldbox
-      font = {
-        name = "Noto fonts";
-        package = pkgs.noto-fonts;
-        size = 14;
-      };
-    };
-    vscode.enable = true;
     impermanence.enable = true;
   };
 }
