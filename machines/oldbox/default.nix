027f5f415498c05e74921eb6fff9c0ecfc16c35b
Jan Kaifer <jan@kaifer.cz>
2025-03-12 01:00:33 +0100

    wip

--- machines/oldbox/default.nix
+++ machines/oldbox/default.nix
@@ -127,10 +127,13 @@ in
               '';
           in
           [
-            "${configFile}:/config/config.yml"
             "/persist/containers/frigate/config:/config"
+            "${configFile}:/frigate_config.yml:ro"
             "/nas/frigate:/media/frigate"
           ];
+        environment = {
+          FRIGATE_CONFIG_FILE = "/frigate_config.yml";
+        };
         extraOptions = [
           "--tmpfs=/tmp/cache:rw,size=1000000000" # 1GB of memory, reduces SSD/SD Card wear
         ];
@@ -150,28 +153,8 @@ in
 
   # Erase root partition
   boot.initrd.postDeviceCommands = lib.mkBefore ''
-    # I copied this from https://mt-caret.github.io/blog/posts/2020-06-29-optin-state.html
-    mkdir -p /mnt
-
-    # We first mount the btrfs root to /mnt
-    # so we can manipulate btrfs subvolumes.
     mount -o subvol=/ /dev/disk/by-label/nixos /mnt
 
-    # While we're tempted to just delete /root and create
-    # a new snapshot from /root-blank, /root is already
-    # populated at this point with a number of subvolumes,
-    # which makes `btrfs subvolume delete` fail.
-    # So, we remove them first.
-    #
-    # /root contains subvolumes:
-    # - /root/var/lib/portables
-    # - /root/var/lib/machines
-    #
-    # I suspect these are related to systemd-nspawn, but
-    # since I don't use it I'm not 100% sure.
-    # Anyhow, deleting these subvolumes hasn't resulted
-    # in any issues so far, except for fairly
-    # benign-looking errors from systemd-tmpfiles.
     btrfs subvolume list -o /mnt/root |
     cut -f9 -d' ' |
     while read subvolume; do
